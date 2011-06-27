391f0c525af8768f960961a88cc618f4c282f45d
slik <slik.jay@gmail.com>
2011-06-27 18:49:13 +0300

    added inputs

--- Source/Browser.Features.js
+++ Source/Browser.Features.js
@@ -43,127 +43,71 @@ BF.add = function(name, func, cache){
 }
 
 var tags = {
+	canvas: function(){
+		return !!document.createElement('canvas').getContext;
+	},
+
 	video: function(){
 		return !!document.createElement("video").canPlayType;
 	},
 
 	audio: function(){
 		return !!document.createElement("audio").canPlayType;
+	},
+
+	command: function(){
+		return 'type' in document.createElement('command');
+	},
+
+	datalist: function(){
+		return 'options' in document.createElement('datalist');
+	},
+
+	details: function(){
+		return 'open' in document.createElement('details');
+	},
+
+	device: function(){
+		return 'type' in document.createElement('device');
+	},
+
+	meter: function(){
+		return 'value' in document.createElement('meter');
+	},
+
+	output: function(){
+		return 'value' in document.createElement('output');
+	},
+
+	progress: function(){
+		return 'value' in document.createElement('progress');
+	},
+
+	time: function(){
+		return 'valueAsDate' in document.createElement('time');
+	},
+
+	form: function(type){
+		var form = document.createElement('form');
+		switch(type){
+			case 'validation': return 'noValidate' in form; break;
+		}
+	},
+
+	iframe: function(type){
+		return type in document.createElement('iframe');
+	},
+
+	input: function(type){
+		return type in document.createElement('input');
 	}
+
 };
 
 BF.add('tag', function(name){
-	if(tags[name]) return tags[name]();
+	if(tags[name]){
+		return tags[name](Array.prototype.slice.call(arguments, 1));
+	}
 }, true);
 
-/*
-var el = {
-	canvas: document.createElement('canvas'),
-    video: document.createElement("video"),
-    audio: document.createElement("audio"),
-    input: document.createElement("input"),
-	iframe: document.createElement('iframe')
-}
-var BF = Browser.Features;
-
-BF.extend = function(obj){
-    for (var i in obj){
-        this[i] = obj[i];
-    }
-}
-
-BF.extend({
-
-	tag: {
-		canvas: !!el.canvas.getContext,
-
-		video: !!el.video.canPlayType,
-		audio: !!el.audio.canPlayType,
-		command: 'type' in document.createElement('command'),
-		datalist: 'options' in document.createElement('datalist'),
-		details: 'open' in document.createElement('details'),
-		device: 'type' in document.createElement('device'),
-		meter: 'value' in document.createElement('meter'),
-		output: 'value' in document.createElement('output'),
-		progress: 'value' in document.createElement('progress'),
-		time: 'valueAsDate' in document.createElement('time'),
-		form: {
-			validation: 'noValidate' in document.createElement('form')
-		},
-		iframe: {
-			sandbox: 'sandbox' in el.iframe,
-			srcdoc: 'srcdoc' in el.iframe
-		},
-		input: {
-			placeholder: ('placeholder' in el.input),
-			autofocus: ('autofocus' in el.input),
-			type: {}
-		}
-	},
-
-	media: {},
-
-    localStorage: (('localStorage' in window) && window['localStorage'] !== null),
-    workers: !!window.Worker,
-    applicationCache: !!window.applicationCache,
-    geolocation: !!navigator.geolocation,
-
-    microdata: !!document.getItems
-
-});
-
-var Inputs = [
-    'search',
-    'number',
-    'range',
-    'color',
-    'tel',
-    'url',
-    'email',
-    'date',
-    'month',
-    'week',
-    'time',
-    'datetime',
-    'datetime-local'
-];
-
-for (var i in Inputs){
-    if(!Inputs.hasOwnProperty(i)) continue;
-    el.input.setAttribute("type", Inputs[i]);
-    BF.tag.input.type[Inputs[i]] = !!(el.input.type !== "text");
-}
-
-var Mimes = {
-	audio: {
-		mp3: 'audio/mpeg;',
-		ogg: 'audio/ogg; codecs="vorbis"',
-		wav: 'audio/wav; codecs="1"',
-		aac: 'audio/mp4; codecs="mp4a.40.2"'
-	},
-	video: {
-		h264: 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"',
-		ogg: 'video/ogg; codecs="theora, vorbis"',
-		webm: 'video/webm; codecs="vp8, vorbis"'
-	}
-}
-
-BF.extend({
-
-    canvasText: (function(){
-        if (!BF.canvas) { return false; }
-        return typeof el.canvas.getContext('2d').fillText == 'function';
-    })()
-
-});
-
-for (var context in Mimes){
-	if(BF[context]){
-		BF.media[context] = {};
-		for (var i in Mimes[context]){
-			BF.media[context][i] = el[context].canPlayType(Mimes[context][i]).replace(/no/, '');
-		}
-	}
-}
-*/
 })();
